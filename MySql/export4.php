65318665bba28197244f85d2fd686ad5a27b2019
ztk <1136717125@qq.com>
2020-11-04 10:44:50 +0800

    'a'

--- MySql/export4.php
+++ MySql/export4.php
@@ -12,7 +12,7 @@ try {
 
 $sql2 = "select t1.name,t1.openid,t1.phone,t1.reservation_id,t1.status,t1.create_time,t2.store_id,t2.date from rzj_store.cmf_reservation_order t1 
 LEFT JOIN rzj_store.cmf_reservation t2 on 
-t1.reservation_id=t2.id where t1.create_time>='2020-01-01 00:00:00' and  t1.create_time<='2020-06-29 23:59:59' order by name desc;";
+t1.reservation_id=t2.id where t1.create_time>='2020-01-01 00:00:00' and  t1.create_time<='2020-10-28 23:59:59' order by name desc;";
 $stmt = $conn->prepare($sql2);
 $stmt->execute();
 // 设置结果集为关联数组
@@ -50,19 +50,33 @@ foreach ($data as &$v){
     $v['store_name'] = isset($storeData[$v['store_id']])?$storeData[$v['store_id']]:'';
 
 }
-$result = $data;
+$result = array();
+foreach ($data as $k=>$v){
+    $result[$k]['name'] =  $v['name'];
+    $result[$k]['openid'] =  $v['openid'];
+    $result[$k]['phone'] =  $v['phone'];
+    $result[$k]['store_id'] =  $v['store_id'];
+    $result[$k]['store_name'] =  $v['store_name'];
+    $result[$k]['date'] =  $v['date'];
+    $result[$k]['status_str'] =  $v['status_str'];
+    $result[$k]['isfuwu'] =  $v['isfuwu'];
+}
 //var_dump($result);
 $excel = new PHPExcelTools();
-$fileName = "统计客户预约服务";
-$headArr=array('name'=>'用户','openid'=>'opend ID',
-    'phone'=>'电话号码','store_id'=>'店铺号',
+$fileName = "2020-01-01至2020-10-28客户预约服务";
+$headArr=array(
+    'name'=>'用户',
+    'openid'=>'opend ID',
+    'phone'=>'电话号码',
+    'store_id'=>'店铺号',
     'store_name'=>'店铺名称',
     'date' => '预约日期',
     'status_str' => '预约状态',
     'isfuwu' => '是否已服务',
 
     );
-$excel->export($fileName,$headArr,$result);
+
+$excel->exportCSV($fileName,$headArr,$result);
 $conn = null;
 
 
